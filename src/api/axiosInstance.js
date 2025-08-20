10ea1519e7dbb4271fc457da3068dbb14f197344
guinwogbegno koffi <koffi.guinwogbegno@ipnetinstitute.com>
2025-08-20 15:14:03 +0000

    les modif de la prod

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://myhospital.archipel-dutyfree.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/api/specialitesApi.js
+++ src/api/specialitesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://myhospital.archipel-dutyfree.com/api';
 
 const getToken = () => localStorage.getItem('token');
 
